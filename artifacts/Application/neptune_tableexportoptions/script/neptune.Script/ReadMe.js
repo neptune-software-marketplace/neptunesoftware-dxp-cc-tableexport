564f48cc29f3d30a29f6bc3178d964925d55bb3c
Lloyd Trevarthen <lloyd.trevarthen@neptune-software.com>
2024-01-04 16:00:34 +0000

    Readme added

---
+++ artifacts/Application/neptune_tableexportoptions/script/neptune.Script/ReadMe.js
@@ -0,0 +1,18 @@
+/*
+Documentation:
+
+The export functionality in this component is built on top of the library called "Table export" 
+which has dependencies such as html2canvas, pdfmaker, filesaver, etc. 
+You can learn more about this library by visiting the GitHub repository at https://github.com/hhurz/tableExport.jquery.plugin/.
+
+This custom component allows you to export a table to various formats 
+such as PDF, CSV, XLSX, and JSON. To make it work, you need to add a table name 
+as seen in the JavaScript file using the following syntax: `$(tableName).tableExport({})`.
+
+If you encounter any errors, make sure to check the responsivepopover to ensure it is correctly referenced.
+
+For a better understanding of custom components and their usage, 
+you can refer to the documentation at https://docs.neptune-software.com/neptune-dxp-open-edition/23/cockpit-overview/app-designer-custom-component.html.
+
+
+*/
